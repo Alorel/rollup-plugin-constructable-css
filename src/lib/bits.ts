0d290efff8237f9c099215c342c16a22b1a14670
Arturas <a.molcanovas@gmail.com>
2020-09-22 02:13:32 +0100

    tests

--- src/lib/bits.ts
+++ src/lib/bits.ts
@@ -1,11 +1,12 @@
 /** @internal */
-export const CLOSING_BRACKET_AND_EXPORT = `);
-
-export default stylesheet;
-`;
+export const CLOSING_BRACKET_AND_EXPORT: string = [
+  ');',
+  '',
+  'export default stylesheet;',
+  ''
+].join('\n');
 
 /** @internal */
 export function stylesheetInit(varType: string): string {
-  return `${varType} stylesheet = new CSSStyleSheet();
-stylesheet.replaceSync(`;
+  return `${varType} stylesheet = new CSSStyleSheet();\nstylesheet.replaceSync(`;
 }
